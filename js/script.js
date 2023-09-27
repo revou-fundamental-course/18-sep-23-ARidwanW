9ee1e421052793d517e168c96e4a82edd21e80d6
ARidwanW <alwandiaridwan@gmail.com>
2023-09-27 19:47:53 +0700

    Checkpoint: image slider

--- js/script.js
+++ js/script.js
@@ -7,10 +7,11 @@
 // * validate nama
 function validateName() {
     // var nameValue = document.getElementById("your-name").value;
-    const nameValue = document.forms["contact-form"]["your-name"].value;
+    let nameValue = document.forms["contact-form"]["your-name"].value;
 
-    if (nameValue === "" ) {
-        document.getElementById("error-name").innerHTML = "Nama tidak boleh kosong!";
+    if (nameValue == "" ) {
+        // document.getElementById("error-name").innerHTML = "Nama tidak boleh kosong!";
+        document.getElementById("error-name").innerText = "Nama tidak boleh kosong!";
         return false;
     }
 
@@ -21,14 +22,16 @@ function validateName() {
 // * validate email
 function validateEmail() {
     // var emailValue = document.getElementById("email").value;
-    const emailValue = document.forms["contact-form"]["email"].value;
+    let emailValue = document.forms["contact-form"]["email"].value;
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-    if (emailValue === ""){
-        document.getElementById("error-email").innerHTML = "Email tidak boleh kosong!";
+    if (emailValue == ""){
+        // document.getElementById("error-email").innerHTML = "Email tidak boleh kosong!";
+        document.getElementById("error-email").innerText = "Email tidak boleh kosong!";
         return false;
     } else if (!emailRegex.test(emailValue)) {
-        document.getElementById("error-email").innerHTML = "Email tidak valid!";
+        // document.getElementById("error-email").innerHTML = "Email tidak valid!";
+        document.getElementById("error-email").innerText = "Email tidak valid!";
         return false;
     }
 
@@ -39,7 +42,7 @@ function validateEmail() {
 // * validasi interest (not required)
 function validateInterest() {
     // var interestValue = document.getElementById("interest").value;
-    const interestValue = document.forms["contact-form"]["interest"].value;
+    let interestValue = document.forms["contact-form"]["interest"].value;
 
     // * not requred
     return true;
@@ -50,21 +53,25 @@ function removeError() {
     var errorEmail = document.getElementById("error-email");
     var errorInterest = document.getElementById("error-interest");
 
-    errorName.innerHTML = "";
-    errorEmail.innerHTML = "";
-    errorInterest.innerHTML = "";
+    // errorName.innerHTML = "";
+    // errorEmail.innerHTML = "";
+    // errorInterest.innerHTML = "";
+
+    errorName.innerText = "";
+    errorEmail.innerText = "";
+    errorInterest.innerText = "";    
 
     return true;
 }
 
 function removeValue() {
-    const nameValue = document.forms["contact-form"]["your-name"].value;
-    const emailValue = document.forms["contact-form"]["email"].value;
-    const interestValue = document.forms["contact-form"]["interest"].value;
+    let nameValue = document.forms["contact-form"]["your-name"];
+    let emailValue = document.forms["contact-form"]["email"];
+    let interestValue = document.forms["contact-form"]["interest"];
 
-    nameValue.innerHTML = "";
-    emailValue.innerHTML = "";
-    interestValue.innerHTML = "";
+    nameValue.innerText = "";
+    emailValue.innerText = "";
+    interestValue.innerText = "";
 }
 
 
@@ -76,8 +83,6 @@ function validateForm() {
     var emailValid = validateEmail();
     var interestValid = validateInterest();
 
-    // return nameValid && emailValid && interestValid;
-
     // cek semua validasi
     if (nameValid && emailValid && interestValid) {
         showSuccessOverlay();
@@ -88,7 +93,10 @@ function validateForm() {
 
 function showSuccessOverlay() {
     var sendSuccess = document.getElementById("send-success");
-    sendSuccess.innerHTML = "Terima Kasih! Data anda telah kami terima. Silahkan tunggu beberapa saat." +
+    // sendSuccess.innerHTML = "Terima Kasih! Data anda telah kami terima. Silahkan tunggu beberapa saat." +
+    //                         " Apabila sales belum menghubungi 1x24 jam setelah anda mengirimkan data" +
+    //                         " Silahkan input ulang data."; 
+    sendSuccess.innerText = "Terima Kasih! Data anda telah kami terima. Silahkan tunggu beberapa saat." +
                             " Apabila sales belum menghubungi 1x24 jam setelah anda mengirimkan data" +
                             " Silahkan input ulang data."; 
 
@@ -100,129 +108,127 @@ function showSuccessOverlay() {
     });
 }
 
+// ??validation form tambahan tapi bugged
+// document.getElementById("send-button").addEventListener("click", function() {
+//     removeError();
+
+//     var nameValid = validateName();
+//     var emailValid = validateEmail();
+//     var interestValid = validateInterest();
+
+//     if (nameValid && emailValid && interestValid) {
+//         removeError();
+//         removeValue()
+//         showSuccessOverlay();
+//     }
+    
+// });
 
 
-// ! slider image
-var slideIndex = 1;
-// showDivsContentLeft(slideIndex);
-// showDivsImage(slideIndex);
-// showDivsTag(slideIndex);
-// showDivsCustomer(slideIndex);
-// showDivsJob(slideIndex);
-// showDivsTopPackage(slideIndex);
-// showDivsBottomPackage(slideIndex);
 
-function plusDivs(n) {
-    showDivsPackage(slideIndex += n);
-    // showDivsImage(slideIndex += n);
-    // showDivsTag(slideIndex += n);
-    // showDivsCustomer(slideIndex += n);
-    // showDivsJob(slideIndex += n);
+// ! ===== slider image =======
+var slideIndexContentLeft = 1;
+var slideIndexTopPackage = 1;
+var slideIndexButtomPackage = 1;
+var intervalID;
+
+showDivsContentLeft(slideIndexContentLeft);
+// showDivsTopPackage(slideIndexTopPackage);
+
+var mobilePixels = window.matchMedia("(max-width: 600px)");
+
+mobilePixels.addEventListener("change", sliderMobile)
+sliderMobile(mobilePixels);
+
+
+// ! jalankan slider jika width dibawah 600px untuk mobile
+function sliderMobile(mobilePixels) {
+    if (mobilePixels.matches) {
+        showDivsTopPackage(slideIndexTopPackage);
+        showDivsBottomPackage(slideIndexButtomPackage)
+
+        // * mulai interval jika belum berjalan
+        if (!intervalID){
+            intervalID = setInterval(() => {
+                plusDivsPackage(1);
+            }, 2000);
+        }
+    } else {
+        resetDivsPackage();
+
+        // * hentikan interval jika sedang berjalan
+        if (intervalID) {
+            clearInterval(intervalID);
+            intervalID = null;      // * atur kembali interval jadi null
+        }
+    }
 }
+
+// ! auto slider content left
+setInterval(() => {
+    plusDivsContentLeft(1);
+}, 3000);
+
+
+function plusDivsContentLeft(n) {
+    showDivsContentLeft(slideIndexContentLeft += n);
+}
+
+function plusDivsPackage(n) {
+    showDivsTopPackage(slideIndexTopPackage += n);
+    showDivsBottomPackage(slideIndexButtomPackage += n);
+}
+
 
 // * content left
 function showDivsContentLeft(n) {
     var x = document.getElementsByClassName("content__left");
-    if (n > x.length) {slideIndex = 1;}
-    if (n < 1) {slideIndex = x.length;}
+    if (n > x.length) {slideIndexContentLeft = 1;}
+    if (n < 1) {slideIndexContentLeft = x.length;}
 
     for (let i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
 
-    x[slideIndex-1].style.display = "block";
+    x[slideIndexContentLeft - 1].style.display = "flex";
 }
 
-// * Package
-function showDivsPackage(n) {
-    var x = document.getElementsByClassName("slider-image");
-    if (n > x.length) {slideIndex = 1;}
-    if (n < 1) {slideIndex = x.length;}
-
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-
-    x[slideIndex-1].style.display = "block";
-}
-
-// * image
-function showDivsImage(n) {
-    var x = document.getElementsByClassName("slider-image");
-    if (n > x.length) {slideIndex = 1;}
-    if (n < 1) {slideIndex = x.length;}
-
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-
-    x[slideIndex-1].style.display = "block";
-}
-
-// * Tag
-function showDivsTag(n) {
-    var x = document.getElementsByClassName("slider-tag");
-    if (n > x.length) {slideIndex = 1;}
-    if (n < 1) {slideIndex = x.length;}
-
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-
-    x[slideIndex-1].style.display = "block";
-}
-
-// * Customer
-function showDivsCustomer(n) {
-    var x = document.getElementsByClassName("slider-customer");
-    if (n > x.length) {slideIndex = 1;}
-    if (n < 1) {slideIndex = x.length;}
-
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-
-    x[slideIndex-1].style.display = "block";
-}
-
-// * Job
-function showDivsJob(n) {
-    var x = document.getElementsByClassName("slider-job");
-    if (n > x.length) {slideIndex = 1;}
-    if (n < 1) {slideIndex = x.length;}
-
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-
-    x[slideIndex-1].style.display = "block";
-}
-
-// * BottomPackage
-function showDivsBottomPackage(n) {
-    var x = document.getElementsByClassName("slider-job");
-    if (n > x.length) {slideIndex = 1;}
-    if (n < 1) {slideIndex = x.length;}
-
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-
-    x[slideIndex-1].style.display = "block";
-}
-
-// * TopPackage
+// * top Package
 function showDivsTopPackage(n) {
-    var x = document.getElementsByClassName("slider-job");
-    if (n > x.length) {slideIndex = 1;}
-    if (n < 1) {slideIndex = x.length;}
+    var x = document.getElementsByClassName("top-package");
+    if (n > x.length) {slideIndexTopPackage = 1;}
+    if (n < 1) {slideIndexTopPackage = x.length;}
 
     for (let i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
 
-    x[slideIndex-1].style.display = "block";
+    x[slideIndexTopPackage - 1].style.display = "flex";
 }
+
+// * bottom Package
+function showDivsBottomPackage(n) {
+    var x = document.getElementsByClassName("bottom-package");
+    if (n > x.length) {slideIndexButtomPackage = 1;}
+    if (n < 1) {slideIndexButtomPackage = x.length;}
+
+    for (let i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+
+    x[slideIndexButtomPackage - 1].style.display = "flex";
+}
+
+function resetDivsPackage() {
+    var packagePoint = document.getElementsByClassName("package__point-container")
+
+    for (let i = 0; i < packagePoint.length; i++) {
+        packagePoint[i].style.display = "flex";
+    }
+}
+
+
+
 
 
 
